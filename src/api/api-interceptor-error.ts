69d9b93617e5808bc915a2c1cfc567ae0d1118c4
Dmytro Kuziaiev <dmytro.kuziaiev@fujitsu.com>
2026-03-05 09:40:14 +0100

    chore: add common error translation

--- src/api/api-interceptor-error.ts
+++ src/api/api-interceptor-error.ts
@@ -1,8 +1,13 @@
+import globalAxios from 'axios';
+import i18next from 'i18next';
+import { toast } from 'react-toastify';
+import { errorToastConfig } from '../config/toast-notification';
+
 export const setupInterceptors = () => {
   globalAxios.interceptors.response.use(
     (response) => response,
     (error) => {
-      const message = error.response?.data?.message || i18next.t('errors.api.unknown_error');
+      const message = error.response?.data?.message || i18next.t('common.unknown_error');
       toast(message, errorToastConfig);
       return Promise.reject(error);
     }
--- src/i18n/ja/common.ts
+++ src/i18n/ja/common.ts
@@ -10,4 +10,5 @@ export default {
   },
   date_format: 'yyyy/MM/dd HH:mm:ss',
   no_results_found: '結果が見つかりませんでした',
+  unknown_error: '「不明なエラーが発生しました」',
 };
